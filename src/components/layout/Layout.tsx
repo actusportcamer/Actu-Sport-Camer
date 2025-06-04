d93fbf69a3cadefcc2b57854a8f84a6450af1438
Coderj1 <ramseslove52@gmail.com>
2025-06-04 01:16:34 +0100

    first comment

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,12 @@
+import { useRef, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 import pow from '../../img/powered.png'
 import VideoPopup from '../VideoPopup';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout() {
+
   return (
     <div className="flex flex-col min-h-screen">
       <img src={pow} className='sm:hidden inline h-36' />
@@ -15,7 +16,6 @@ export default function Layout() {
       </main>
       <Footer />
       <VideoPopup />
-      <SpeedInsights />
     </div>
   );
 }
