e3616a16e61f6935c6b846cd64b97699565288c5
Coderj <ramseslove52@gmail.com>
2025-07-21 19:40:25 +0100

    Update

--- src/components/blog/FeaturedPostsSection.tsx
+++ src/components/blog/FeaturedPostsSection.tsx
@@ -24,7 +24,7 @@ export default function FeaturedPostsSection({ posts }: FeaturedPostsSectionProp
           </Link>
         </div>
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {posts.map((post) => (
             <PostCard key={post.id} post={post} featured />
           ))}
--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,48 +1,109 @@
-import React from 'react';
 import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Container from '../ui/Container';
 import Button from '../ui/Button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, Autoplay } from 'swiper/modules';
+import { Query } from 'appwrite';
+import React, { useEffect, useState } from 'react';
+import { databases } from '../../AppwriteConfig'
 
 export default function HeroSection() {
+
+   const [ featuredblogs, setFeaturedBlog] = useState([])
+
+  useEffect(() => {
+    const getBlog = async () => {
+      try {
+        const response = await databases.listDocuments(
+        '68379f30000f7d86e98d',       // Replace with your Appwrite database ID
+        '68379fa2002f31d6d937',     // Replace with your Appwrite collection ID
+          [
+            Query.orderDesc('publishedAt'),
+            Query.limit(5)
+          ]
+        );
+        setFeaturedBlog(response.documents); // Returns an array of documents
+      } catch (error) {
+        console.error("Error fetching collection:", error);
+      }
+    }
+    getBlog();
+  }, []);
+
   return (
-    <section className="pt-8 pb-16 md:pt-16 md:pb-20 bg-gradient-to-br from-white to-purple-100">
-      <Container>
-        <div className="flex gap-8 flex-col md:flex-row items-center">
-          <div className="md:mt-0 md:w-1/2 md:pr-12">
-            <div className="relative">
-              <div className="absolute inset-0 bg-blue-800 rounded-2xl transform rotate-3 scale-105 opacity-15"></div>
-              <div className="absolute inset-0 bg-purple-800 rounded-2xl transform -rotate-8 scale-105 opacity-15"></div>
+    <>
+    {/*
+      <section className="pt-8 pb-16 md:pt-16 md:pb-20 bg-gradient-to-br from-white to-purple-100">
+        <Container>
+          <div className="flex gap-8 flex-col md:flex-row items-center">
+            <div className="md:mt-0 md:w-1/2 md:pr-12">
+              <div className="relative">
+                <div className="absolute inset-0 bg-blue-800 rounded-2xl transform rotate-3 scale-105 opacity-15"></div>
+                <div className="absolute inset-0 bg-purple-800 rounded-2xl transform -rotate-8 scale-105 opacity-15"></div>
+                <img
+                  src="https://magazin.hockey.de/uploads/media/image-1920/04/42214-FU1_2284%20%282%29.webp?v=1-1"
+                  alt="People collaborating on web development"
+                  className="rounded-2xl shadow-xl relative z-10 w-full h-auto"
+                />
+              </div>
+            </div>
+            <div className="md:w-1/2 mt-0">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 leading-tight">
+              Your front-row seat to the latest news in sports
+              </h1>
+              <p className="mt-6 text-md text-gray-800 leading-relaxed">
+              news, insights, and stories that move the game forward so stay tune and get informed
+              </p>
+              <div className="mt-8 flex flex-wrap gap-4">
+                <Link to="/blog">
+                  <Button variant="primary" size="md" className="group">
+                    Explore Articles
+                    <ArrowRight size={18} className="ml-2 transition-transform group-hover:translate-x-1" />
+                  </Button>
+                </Link>
+                <Link className='sm:inline hidden' to="/category/Football">
+                  <Button variant="outline" size="md">
+                    Browse Categories
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
+     */}
+        <div className="">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000 }}
+        loop={true}
+      >
+        {featuredblogs.map((post, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative overflow-hidden shadow-lg">
+              <Link to={`/blog/${post.$id}`}>
               <img
-                src="https://magazin.hockey.de/uploads/media/image-1920/04/42214-FU1_2284%20%282%29.webp?v=1-1"
-                alt="People collaborating on web development"
-                className="rounded-2xl shadow-xl relative z-10 w-full h-auto"
+                src={post.coverImage}
+                alt={post.title}
+                className="w-full h-[630px] object-cover"
               />
-            </div>
-          </div>
-          <div className="md:w-1/2 mt-0">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 leading-tight">
-            Your front-row seat to the latest news in sports
-            </h1>
-            <p className="mt-6 text-md text-gray-800 leading-relaxed">
-            news, insights, and stories that move the game forward so stay tune and get informed
-            </p>
-            <div className="mt-8 flex flex-wrap gap-4">
-              <Link to="/blog">
-                <Button variant="primary" size="md" className="group">
-                  Explore Articles
-                  <ArrowRight size={18} className="ml-2 transition-transform group-hover:translate-x-1" />
-                </Button>
-              </Link>
-              <Link className='sm:inline hidden' to="/category/Football">
-                <Button variant="outline" size="md">
-                  Browse Categories
-                </Button>
+              <div className="absolute bottom-0 bg-black/70 text-white p-8  w-full font-semibold">
+                <p className='px-2.5 py-0.5 mb-2 w-20 bg-blue-50 text-blue-700 text-center rounded-full hover:bg-blue-100 transition-colors'>{post.category}</p>
+                <p className='md:text-5xl text-2xl line-clamp-2'>{post.title}</p>
+                <p className='text-md line-clamp-2 text-gray-400 md:w-[1000px]'>{post.excerpt}</p>
+              </div>
               </Link>
             </div>
-          </div>
-        </div>
-      </Container>
-    </section>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+    </>
   );
 }
